f3d8a1d09514d6004096bb644a59250bba352078
Lorenzo Muñoz <lorenzojosemunoz@hotmail.com>
2018-05-22 11:54:36 -0300

    estructura

---
+++ src/about/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="seven">
+                    <div className="center">
+                        Nunca dejes de buscar
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default About;
